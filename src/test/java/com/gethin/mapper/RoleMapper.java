46bc71878c8f20ca6fa10169bb67b382ea3f0243
terui <terui@pinduoduo.com>
2020-08-21 17:18:10 +0800

    feat: 新增mybatis测试用例 & 格式化

---
+++ src/test/java/com/gethin/mapper/RoleMapper.java
@@ -0,0 +1,13 @@
+package com.gethin.mapper;
+
+import com.gethin.po.Role;
+
+public interface RoleMapper {
+    public Role getRole(Long id);
+
+    public Role findRole(String roleName);
+
+    public int deleteRole(Long id);
+
+    public int insertRole(Role role);
+}
